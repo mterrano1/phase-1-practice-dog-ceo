64de862bb913d0da08b5c20166ea905d8d44dc72
Christopher Terrano <mterrano1@yahoo.com>
2021-12-09 17:16:19 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', getDogPics);
+
+function getDogPics(){
+    fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(res => res.json())
+    .then(data => {
+        let dogList = data.message
+        dogList.forEach(dog => renderDogImg(dog))
+    })
+};
+
+function renderDogImg(dog){
+    const div = document.querySelector('div');
+    const dogImg = document.createElement('img');
+    dogImg.src = dog
+        div.appendChild(dogImg);
+        dogImg.width = 200;
+        dogImg.height = 200;
+};
+
+function getBreeds(){
+    fetch('https://dog.ceo/api/breeds/list/all')
+    .then(res => res.json())
+    .then(data => {
+        let breedList = data.message
+        for (breed in breedList){
+            renderBreed(breed)
+        }
+    })
+};
+
+function renderBreed(breed){
+    const ul = document.querySelector('#dog-breeds');
+    const li = document.createElement('li');
+    li.innerText = breed
+    ul.appendChild(li)
+};
+
+getBreeds();
